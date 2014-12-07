ce045ae9ca9ee3fe682adf51360f4a0292d2bf4c
Darren Thomas <darrenthomas5@users.noreply.github.com>
2014-12-07 13:27:47 +0000

    Update radioPi.py

--- radioPi.py
+++ radioPi.py
@@ -10,37 +10,47 @@ GPIO.setup(18, GPIO.IN)
 #Start mpd
 #os.system("mpd") #Already running on startup
 
-#Scan for bbc radio & add stations to playlist
-os.system("./bbcRadio")
-
-#Start mpc
-os.system("mpc play")
-
 #Set variables
 pause = False
+radio = False
+
+#Clear mpc, load and play
+os.system("mpc clear")
+os.system("mpc load songs")
+os.system("mpc play")
 
 #Buttons
 while True:
-
+	
 	#Assign buttons to GPIO input pins
-
 	pauseButton = GPIO.input(18)
 	prevButton = GPIO.input(11)
 	nextButton = GPIO.input(12)
 
-	#Prevous button, if paused scan for radio stations else skip to prevous song/station
-
+	#Previous button, if paused switch to radio stations else skip to previous song/station
 	if prevButton == False:
 		if pause == True:
-			pause = False
-			os.system("./bbcRadio")
-			os.system("mpc play")
+			if radio == True:
+				pause = False
+				radio = False
+				os.system("mpc clear")
+				os.system("mpc load songs")
+				os.system("mpc random")
+				os.system("mpc play")
+				time.sleep(.2)
+			elif pause == False:
+				pause = False
+				radio = True
+				os.system("mpc clear")
+				os.system("mpc random")
+				os.system("mpc load radio")
+				os.system("mpc play")
+				time.sleep(.2)
 		elif pause == False:
 			os.system("mpc prev")
 			time.sleep(.2)
-
+			
 	#Next button, if paused stop player and shutdown else skip to next song/station
-
 	elif nextButton == False:
 		if pause == True:
 			os.system("mpc stop")
@@ -50,7 +60,6 @@ while True:
 			time.sleep(.2)
 
 	#Pause button, toggle pause on off
-
 	elif pauseButton == False:
 		os.system("mpc toggle")
 		if pause == False:
